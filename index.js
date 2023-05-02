31af7762d90ec5eba1fa74d92ab36a63f925fa02
Azazel-Foxwood <redseabass95@gmail.com>
2023-05-01 21:14:52 -0500

    printed an array of only the drinks

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const coffeeMenu = require("./coffee_data.js");
+
+// Print an array of all the drinks on the menu
+
+const printName = (drinks) => {
+    return drinks.name;
+}
+console.log(coffeeMenu.map(printName));
